a621fa9d77bad14ca320ec11c7b4f0b2762d4289
solareenlo <solareenlo@gmail.com>
2019-01-14 20:03:16 +0900

    ちょっと追記

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,5 +1,6 @@
 import * as shell from 'shelljs';
 
+shell.mkdir('-p', 'dist/public/js/lib/');
 shell.cp('-R', 'src/public/js/lib', 'dist/public/js/');
 shell.cp('-R', 'src/public/webfonts', 'dist/public/');
 shell.cp('-R', 'src/public/images', 'dist/public/');
