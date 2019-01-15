f2750d208a6d28bc474e87fb7c72857492d6556a
solareenlo <solareenlo@gmail.com>
2019-01-15 17:32:24 +0900

    assertをcheckに変換

--- src/app.ts
+++ src/app.ts
@@ -90,7 +90,7 @@ app.use((req, res, next) => {
     && !req.path.match(/\./)) {
     req.session.returnTo = req.originalUrl;
   } else if (req.user
-    && (req.path === '/account' || req.path.match(/^\/api/))) {
+    && req.path === '/account') {
     req.session.returnTo = req.originalUrl;
   }
   next();
--- src/controllers/contact.ts
+++ src/controllers/contact.ts
@@ -21,7 +21,7 @@ export let postContact = (req: Request, res: Response) => {
   let fromName;
   let fromEmail;
   if (!req.user) {
-    req.assert('name', 'Name cannot be blank').notEmpty();
+    req.check('name').notEmpty().withMessage('Name cannot be blank');
     req.assert('email', 'Email is not valid').isEmail();
   }
   req.assert('message', 'Message cannot be blank').notEmpty();
